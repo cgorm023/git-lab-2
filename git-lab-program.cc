0ef5955311e78a80358dd3968373d27edc05cf48
cgorm023 <c.gorm023@gmail.com>
2024-01-25 15:31:59 -0500

    code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Charlie Gorman
+ *        Date: 1/25/24
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
